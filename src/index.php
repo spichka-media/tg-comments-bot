794a137a880307704b9e6fe43a030ae6f8eb6687
John Doe <john.doe@spichka.media>
2024-08-26 16:04:23 +0300

    feat: initial commit

---
+++ src/index.php
@@ -0,0 +1,57 @@
+<?php
+    
+    $env = parse_ini_file('.env');
+
+    $botToken = $env["BOT_TOKEN"];
+    $discussionChatId = intval($env["DISCUSSION_CHAT_ID"]);
+    $lastUpdateId = 0;
+    $comment = $env["COMMENT"];
+
+    $updates = json_decode(
+        file_get_contents("https://api.telegram.org/bot$botToken/getUpdates"),
+        true
+    );
+    
+    foreach ($updates["result"] as $update) {
+        $lastUpdateId = $update["update_id"];
+        if (
+            !empty($update["message"]["is_automatic_forward"])
+        ) {
+            $qwe = http_build_query([
+                "chat_id" => $discussionChatId,
+                "text" => $comment,
+                "parse_mode" => "MarkdownV2",
+                "reply_to_message_id" =>
+                    $update["message"]["message_id"],
+            ]);
+            file_get_contents(
+                "https://api.telegram.org/bot$botToken/sendMessage",
+                false,
+                stream_context_create([
+                    "http" => [
+                        "header" =>
+                            "Content-type: application/x-www-form-urlencoded",
+                        "method" => "POST",
+                        "content" => http_build_query([
+                            "chat_id" => $discussionChatId,
+                            "text" => $comment,
+                            "parse_mode" => "MarkdownV2",
+                            "disable_web_page_preview" => true,
+                            "reply_to_message_id" =>
+                                $update["message"]["message_id"],
+                        ]),
+                    ],
+                ])
+            );
+        }
+    }
+    
+    //Reset updates for further runs
+    if ($lastUpdateId > 0) {
+        file_get_contents(
+            "https://api.telegram.org/bot$botToken/getUpdates?offset=" .
+                ($lastUpdateId + 1)
+        );
+    }
+
+?>
