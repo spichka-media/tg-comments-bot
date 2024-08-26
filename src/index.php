5b5372d455376cac01e6113ed61e17a6436dfdd0
John Doe <john.doe@spichka.media>
2024-08-26 16:12:16 +0300

    fix: remove redundant variables

--- src/index.php
+++ src/index.php
@@ -1,51 +1,37 @@
 <?php
-    
-    $env = parse_ini_file('.env');
 
-    $botToken = $env["BOT_TOKEN"];
-    $discussionChatId = intval($env["DISCUSSION_CHAT_ID"]);
-    $lastUpdateId = 0;
-    $comment = $env["COMMENT"];
+$env = parse_ini_file('.env');
 
-    $updates = json_decode(
-        file_get_contents("https://api.telegram.org/bot$botToken/getUpdates"),
-        true
-    );
-    
-    foreach ($updates["result"] as $update) {
-        $lastUpdateId = $update["update_id"];
-        if (
-            !empty($update["message"]["is_automatic_forward"])
-        ) {
+$lastUpdateId = 0;
 
-            file_get_contents(
-                "https://api.telegram.org/bot$botToken/sendMessage",
-                false,
-                stream_context_create([
-                    "http" => [
-                        "header" =>
-                            "Content-type: application/x-www-form-urlencoded",
-                        "method" => "POST",
-                        "content" => http_build_query([
-                            "chat_id" => $discussionChatId,
-                            "text" => $comment,
-                            "parse_mode" => "MarkdownV2",
-                            "disable_web_page_preview" => true,
-                            "reply_to_message_id" =>
-                                $update["message"]["message_id"],
-                        ]),
-                    ],
-                ])
-            );
-        }
-    }
-    
-    //Reset updates for further runs
-    if ($lastUpdateId > 0) {
+$updates = json_decode(file_get_contents('https://api.telegram.org/bot' . $env['BOT_TOKEN'] . '/getUpdates'), true);
+
+foreach ($updates['result'] as $update) {
+    $lastUpdateId = $update['update_id'];
+    if (!empty($update['message']['is_automatic_forward'])) {
         file_get_contents(
-            "https://api.telegram.org/bot$botToken/getUpdates?offset=" .
-                ($lastUpdateId + 1)
+            'https://api.telegram.org/bot' . $env['BOT_TOKEN'] . '/sendMessage',
+            false,
+            stream_context_create([
+                'http' => [
+                    'header' => 'Content-type: application/x-www-form-urlencoded',
+                    'method' => 'POST',
+                    'content' => http_build_query([
+                        'chat_id' => intval($env['DISCUSSION_CHAT_ID']),
+                        'text' => $env['COMMENT'],
+                        'parse_mode' => 'MarkdownV2',
+                        'disable_web_page_preview' => true,
+                        'reply_to_message_id' => $update['message']['message_id'],
+                    ]),
+                ],
+            ]),
         );
     }
+}
+
+//Reset updates for further runs
+if ($lastUpdateId > 0) {
+    file_get_contents('https://api.telegram.org/bot' . $env['BOT_TOKEN'] . '/getUpdates?offset=' . ($lastUpdateId + 1));
+}
 
 ?>
