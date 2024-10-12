1441e9469c011beb4230d0cf3096083d7d1a3772
John Doe <john.doe@spichka.media>
2024-10-12 23:12:30 +0300

    fix: issues with multiple messages on posts with images

--- src/index.php
+++ src/index.php
@@ -3,12 +3,28 @@
 $env = parse_ini_file('.env');
 
 $lastUpdateId = 0;
+$lastMediaGroupId = null;
 
 $updates = json_decode(file_get_contents('https://api.telegram.org/bot' . $env['BOT_TOKEN'] . '/getUpdates'), true);
 
+if (isset($updates['result']) && count($updates['result']) > 0) {
+    error_log("[INFO] received updates:\n" . json_encode($updates, JSON_PRETTY_PRINT));
+}
+
 foreach ($updates['result'] as $update) {
     $lastUpdateId = $update['update_id'];
-    if (!empty($update['message']['is_automatic_forward'])) {
+
+    if (!empty($update['message']['media_group_id'])) {
+        if ($lastMediaGroupId === $update['message']['media_group_id']) {
+            error_log('[INFO] skipped message with the same media_group_id: ' . $update['message']['media_group_id']);
+            continue;
+        }
+
+        $lastMediaGroupId = $update['message']['media_group_id'];
+    }
+
+    if (!empty($update['message']['is_automatic_forward']) && !empty($update['message']['chat']['id']) && $update['message']['chat']['id'] === intval($env['DISCUSSION_CHAT_ID'])) {
+        error_log('[INFO] Replying on message ' . $update['message']['message_id']);
         file_get_contents(
             'https://api.telegram.org/bot' . $env['BOT_TOKEN'] . '/sendMessage',
             false,
@@ -31,7 +47,6 @@ foreach ($updates['result'] as $update) {
 
 //Reset updates for further runs
 if ($lastUpdateId > 0) {
+    error_log('[INFO] reseting updates after lastUpdateId ' . $lastUpdateId);
     file_get_contents('https://api.telegram.org/bot' . $env['BOT_TOKEN'] . '/getUpdates?offset=' . ($lastUpdateId + 1));
 }
-
-?>
